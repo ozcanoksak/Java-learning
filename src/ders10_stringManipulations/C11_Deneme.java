453c4d629e1c62c9d3fc8ef895f3f0cd90f7d31f
ozcanoksak <zcnoksak@gmail.com>
2026-01-30 00:56:50 +0300

    pr testing

---
+++ src/ders10_stringManipulations/C11_Deneme.java
@@ -0,0 +1,15 @@
+package ders10_stringManipulations;
+
+public class C11_Deneme {
+
+
+
+    public void hello()
+    {
+        int a = 6;
+        int b = 7;
+
+        System.out.println(a + b);
+    }
+
+}
