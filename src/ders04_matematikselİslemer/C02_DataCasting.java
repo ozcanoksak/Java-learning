b07663ac56a4b85711f93f3eb33ea03f7605a41c
ozcanoksak <ozcan.oksak21@gmail.com>
2026-03-01 16:18:56 +0300

    ders04 updated,

--- src/ders04_matematikselİslemer/C02_DataCasting.java
+++ src/ders04_matematikselİslemer/C02_DataCasting.java
@@ -6,7 +6,8 @@ public class C02_DataCasting {
     public static void main(String[] args) {
 
         /* Kullanıcıdan iki double sayı alin, ikinci sayıyı, ilk sayıya bölün ve bölüm işleminin
-        sonucunun tam sayı kısmını yazdırın */
+        sonucunun tam sayı kısmını yazdırın
+        */
 
 
         Scanner scan=new Scanner(System.in);
